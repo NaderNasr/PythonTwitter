dc2fc3a7f90245a2107cd46d94291d56ff9ac276
Nader Nasr <nadernasr7@gmail.com>
2018-06-06 00:25:46 +0300

    Twitter API

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
+import twitter
+
 app = Flask(__name__)
 
 
@@ -8,6 +11,12 @@ def index():
     return render_template('index.html')
 # def hello():
 #     return "Hello World! The server is AAAAlllliiiiivvveeeee"
+
+
+api = twitter.Api(consumer_key='3p6R5GffllmdOFwIQpMe5ZI6I',
+                  consumer_secret='Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W',
+                  access_token_key='328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh',
+                  access_token_secret='EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF')
 
 
 Bootstrap(app)
