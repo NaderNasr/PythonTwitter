acc8d01b891ad36d22e3a4d4eab8e3a340b5ca6d
Nader Nasr <nadernasr7@gmail.com>
2018-06-06 01:02:40 +0300

    get user timeline added

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ from flask_bootstrap import Bootstrap
 
 import twitter
 
-app = Flask(__name__)
+# app = Flask(__name__)
+# Bootstrap(app)
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 # def hello():
 #     return "Hello World! The server is AAAAlllliiiiivvveeeee"
 
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 api = twitter.Api(consumer_key='3p6R5GffllmdOFwIQpMe5ZI6I',
                   consumer_secret='Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W',
                   access_token_key='328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh',
                   access_token_secret='EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF')
 
+generat = api.GetUserTimeline(screen_name="nader_nasr", count=100)
 
-Bootstrap(app)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print(generat)
