8718c4ad9fabd8a79ec58b602fd802a0a4ced3bd
Nader Nasr <nadernasr7@gmail.com>
2018-06-06 02:53:47 +0300

    Integerating Tweepy

--- app.py
+++ app.py
@@ -1,28 +1,59 @@
+import tweepy
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
-import twitter
 
-# app = Flask(__name__)
+app = Flask(__name__)
+
+
 # Bootstrap(app)
-
-
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
+@app.route("/")
 # def hello():
-#     return "Hello World! The server is AAAAlllliiiiivvveeeee"
+#         return "Hello World! The server is AAAAlllliiiiivvveeeee"
+def index():
+    return render_template('index.html')
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-api = twitter.Api(consumer_key='3p6R5GffllmdOFwIQpMe5ZI6I',
-                  consumer_secret='Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W',
-                  access_token_key='328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh',
-                  access_token_secret='EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF')
-
-generat = api.GetUserTimeline(screen_name="nader_nasr", count=100)
+consumer_key = '3p6R5GffllmdOFwIQpMe5ZI6I'
+consumer_secret = 'Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W'
+access_key = '328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh'
+access_secret = 'EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF'
 
 
-print(generat)
+# Function to extract tweets
+def get_tweets(username):
+
+        # Authorization to consumer key and consumer secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # Access to user's access key and access secret
+        auth.set_access_token(access_key, access_secret)
+
+        # Calling api
+        api = tweepy.API(auth)
+
+        # number of tweets to be extracted
+        number_of_tweets = 1
+
+        tweets = api.user_timeline(screen_name=username)
+
+        # Empty Array
+        tmp=[]
+
+        # create array of tweet information: username,
+        # tweet id, date/time, text
+        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created
+        for j in tweets_for_csv:
+
+            # Push tweets to the empty array tmp
+            tmp.append(j)
+
+        # Printing the tweets
+        print(tmp)
+
+
+# Driver code
+if __name__ == '__main__':
+
+    # Here goes the twitter handle for the user
+    # whose tweets are to be extracted.
+    get_tweets("ReBootKAMP")
--- setup.py
+++ setup.py
@@ -1,77 +1,45 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function
-
-#
-# Copyright 2007-2016 The Python-Twitter Developers
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-import re
-import codecs
-
+#from distutils.core import setup
+import re, uuid
 from setuptools import setup, find_packages
 
+VERSIONFILE = "tweepy/__init__.py"
+ver_file = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, ver_file, re.M)
 
-cwd = os.path.abspath(os.path.dirname(__file__))
+if mo:
+    version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-def read(filename):
-    with codecs.open(os.path.join(cwd, filename), 'rb', 'utf-8') as h:
-        return h.read()
-
-metadata = read(os.path.join(cwd, 'twitter', '__init__.py'))
-
-def extract_metaitem(meta):
-    # swiped from https://hynek.me 's attr package
-    meta_match = re.search(r"""^__{meta}__\s+=\s+['\"]([^'\"]*)['\"]""".format(meta=meta),
-                           metadata, re.MULTILINE)
-    if meta_match:
-        return meta_match.group(1)
-    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
-
-setup(
-    name='python-twitter',
-    version=extract_metaitem('version'),
-    license=extract_metaitem('license'),
-    description=extract_metaitem('description'),
-    long_description=(read('README.rst') + '\n\n' +
-                      read('AUTHORS.rst') + '\n\n' +
-                      read('CHANGES')),
-    author=extract_metaitem('author'),
-    author_email=extract_metaitem('email'),
-    maintainer=extract_metaitem('author'),
-    maintainer_email=extract_metaitem('email'),
-    url=extract_metaitem('url'),
-    download_url=extract_metaitem('download_url'),
-    packages=find_packages(exclude=('tests', 'docs')),
-    platforms=['Any'],
-    install_requires=['future', 'requests', 'requests-oauthlib'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    keywords='twitter api',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Communications :: Chat',
-        'Topic :: Internet',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-)
+setup(name="tweepy",
+      version=version,
+      description="Twitter library for python",
+      license="MIT",
+      author="Joshua Roesslein",
+      author_email="tweepy@googlegroups.com",
+      url="http://github.com/tweepy/tweepy",
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          "requests>=2.11.1",
+          "requests_oauthlib>=0.7.0",
+          "six>=1.10.0",
+          "PySocks>=1.5.7",
+      ],
+      keywords="twitter library",
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Topic :: Software Development :: Libraries',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      zip_safe=True)
