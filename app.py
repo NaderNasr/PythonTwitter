61fe99a12ab45ed8a05c2bb34d098b63513d6efa
Nader Nasr <nadernasr7@gmail.com>
2018-06-06 14:19:53 +0300

    get tweets

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
 import tweepy
 
 
-consumer_token = '3p6R5GffllmdOFwIQpMe5ZI6I'
+consumer_key = '3p6R5GffllmdOFwIQpMe5ZI6I'
 consumer_secret = 'Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W'
-access_token = '328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh'
-access_token_secret = 'EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF'
-
-class MyStreamListener(tweepy.StreamListener):
-
-    def on_status(self, status):
-        print(status.text)
+access_key = '328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh'
+access_secret = 'EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF'
 
 
-auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+# Function to extract tweets
+def get_tweets(username):
+        # Authorization to consumer key and consumer secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print('Error! Failed to get request token.')
+        # Access to user's access key and access secret
+        auth.set_access_token(access_key, access_secret)
 
-api = tweepy.API(auth)
+        # Calling api
+        api = tweepy.API(auth)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+        # number of tweets to be extracted
+        number_of_tweets = 5
+        tweets = api.user_timeline(screen_name=username)
+
+        # Empty Array
+        tmp = []
+
+        # create array of tweet information: username,
+        # tweet id, date/time, text
+        tweets_for_csv = [tweet.text for tweet in tweets]
+        # CSV file created
+        for j in tweets_for_csv:
+
+            # Appending tweets to the empty array tmp
+            tmp.append(j)
+        # Printing the tweets
+        print(tmp)
+
+
+# Driver code
+if __name__ == '__main__':
+
+    # Here goes the twitter handle for the user
+    # whose tweets are to be extracted.
+    get_tweets("Nader_Nasr")
