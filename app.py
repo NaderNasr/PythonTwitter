8e3419ed2e8e66456a7fd684c9ab2c64e0cdabd8
Nader Nasr <nadernasr7@gmail.com>
2018-06-06 14:15:41 +0300

    testing

--- app.py
+++ app.py
@@ -1,59 +1,25 @@
 import tweepy
-from flask import Flask, render_template
 
 
-app = Flask(__name__)
-
-
-# Bootstrap(app)
-@app.route("/")
-# def hello():
-#         return "Hello World! The server is AAAAlllliiiiivvveeeee"
-def index():
-    return render_template('index.html')
-
-
-consumer_key = '3p6R5GffllmdOFwIQpMe5ZI6I'
+consumer_token = '3p6R5GffllmdOFwIQpMe5ZI6I'
 consumer_secret = 'Nk4Z5pOGej6CVPD0Q5GPGLZOu6SLMibxjQZzaRMXvflrtwEL3W'
-access_key = '328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh'
-access_secret = 'EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF'
+access_token = '328208831-VFyiBf8qz7yYhSmQaxlefN8uw07i1Tf2g1YKHPlh'
+access_token_secret = 'EFdGZQiGeCTyQawrWcx6ayPEN6Gn5T6MFkiG255ttXdHF'
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
 
 
-# Function to extract tweets
-def get_tweets(username):
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 
-        # Authorization to consumer key and consumer secret
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print('Error! Failed to get request token.')
 
-        # Access to user's access key and access secret
-        auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
-        # Calling api
-        api = tweepy.API(auth)
-
-        # number of tweets to be extracted
-        number_of_tweets = 1
-
-        tweets = api.user_timeline(screen_name=username)
-
-        # Empty Array
-        tmp=[]
-
-        # create array of tweet information: username,
-        # tweet id, date/time, text
-        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created
-        for j in tweets_for_csv:
-
-            # Push tweets to the empty array tmp
-            tmp.append(j)
-
-        # Printing the tweets
-        print(tmp)
-
-
-# Driver code
-if __name__ == '__main__':
-
-    # Here goes the twitter handle for the user
-    # whose tweets are to be extracted.
-    get_tweets("ReBootKAMP")
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
